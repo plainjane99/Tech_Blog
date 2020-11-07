0a5547ddeeb456e930ea0dc6aaa6a8bc909671d3
Plain Jane <jane99hsu@gmail.com>
2020-11-07 12:28:23 -0800

    added User model

--- models/User.js
+++ models/User.js
@@ -1,42 +1,30 @@
-// import the Model class and DataTypes object from Sequelize
-// the Model class is what we create our models from 
+// import Model class and DataTypes object from Sequelize to create our models from 
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 // node package to perform hashing function
 const bcrypt = require('bcrypt');
 
 // create our User model from the Sequelize constructor
-// So the class inherits the Model functionality
 class User extends Model {
     // instance method that takes in the plaintext password retrieved from the client 
-    // request at req.body.email and compares that with the hashed password
-    // set up method to run on instance data (per user) to check password
     checkPassword(loginPw) {
-        // Using the keyword this, we can access this user's properties, 
-        // including the password, which was stored as a hashed string
-        // compareSync confirms (true) or denies (false) that the supplied password
-        // matches the hashed password stored on the object
+        // access this user's properties, i.e. the password, which was stored as a hashed string
+        // compareSync confirms (true) or denies (false) that the supplied password matches the hashed password stored on the object
         return bcrypt.compareSync(loginPw, this.password);
     }
 }
 
-// initialize the model's data and configuration, passing in two objects as arguments
-// define table columns and configuration
-// the .init method provides context as to how those inherited methods should work
+// initialize the model's data and configuration by passing in two objects as arguments
 User.init(
-    // The first object will define the columns and data types for those columns
-    {
-        // define an id column
+// first argument: defines table columns and configuration
+{
+        // define columns with Sequelize DataTypes to define the data type
+        // define an id column 
         id: {
-            // use the special Sequelize DataTypes object to define what type of data it is
             type: DataTypes.INTEGER,
-            // this is the equivalent of SQL's `NOT NULL` option
             allowNull: false,
-            // instruct that this is the Primary Key
             primaryKey: true,
-            // turn on auto increment
             autoIncrement: true
-            // TABLE COLUMN DEFINITIONS GO HERE
         },
         // define a username column
         username: {
@@ -49,8 +37,7 @@ User.init(
             allowNull: false,
             // there cannot be any duplicate email values in this table
             unique: true,
-            // this is a built-in validator in sequelize
-            // if allowNull is set to false, we can run our data through validators before creating the table data
+            // use sequelize built-in validator to check for email format before creating table data
             validate: {
                 isEmail: true
             }
@@ -59,61 +46,29 @@ User.init(
         password: {
             type: DataTypes.STRING,
             allowNull: false,
+            // use sequelize built-in validator to check password is at least 4 characters long
             validate: {
-                // this means the password must be at least four characters long
                 len: [4]
             }
         }
     },
-    // The second object configures certain options for the table.
+    // second argument: configures certain options for the table.
     {
-        // Hashing performs a one-way transformation on a password, 
-        // turning the password into another string, called the hashed password
-        // use an async function that will return a hashed password in a Promise
-        // bcrypt is CPU intensive so we need to allow other functions to run
-        // this method will autogenerate a salt
-        // saltRounds parameter is known as the cost factor and 
-        // controls how many rounds of hashing are done by the bcrypt algorithm
-        // special sequelize functions called hooks (or lifecycle events) that 
-        // are called before or after calls in sequelize
-        // pass in hooks object into the second part of the user.init()
-        // hooks: {
-        //     // set up beforeCreate lifecycle "hook" functionality
-        //     // we need a hook that will fire just before a new instance of user is created
-        //     beforeCreate(userData) {
-        //         // bcrypt.hash(myPlaintextPassword, saltRounds)
-        //         // execute the bcrypt hash function on the   
-        //         // userData object that contains the plaintext password
-        //         // in the password property
-        //         // salt value of 10
-        //         // resulting hashed password is then passed to the Promise object as newUserData
-        //         return bcrypt.hash(userData.password, 10).then(newUserData => {
-        //             // the hashed password is returned 
-        //             return newUserData
-        //         });
-        //     }
-        // },
-
-        // alternate way to handle async functions that will make the code more concise and legible
-        // keyword pair async/await works in tandem
-        // async keyword is used as a prefix to the function that contains the asynchronous function. 
+        // use 'hooks' to call functions before/after calls in sequelize are executed 
         hooks: {
-            // set up beforeCreate lifecycle "hook" functionality
+            // call the 'beforeCreate' function before adding data to database
+            // function hashes the user's password
             async beforeCreate(newUserData) {
-                // await can be used to prefix the async function, 
-                // which will then gracefully assign the value from the response to the newUserData's password property. 
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                // The newUserData is then returned to the application with the hashed password.
                 return newUserData;
             },
-            // set up beforeUpdate lifecycle "hook" functionality prior to an update
+            // call the 'beforeUpdate' function before updaing data in the database
+            // function hashes the user's password
             async beforeUpdate(updatedUserData) {
                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
                 return updatedUserData;
             }
         },
-
-        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
         // pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
