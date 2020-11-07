9dee71c3aeff19be89a9f91811d707c288783479
Plain Jane <jane99hsu@gmail.com>
2020-11-07 12:50:19 -0800

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,50 @@
+// import Model class and DataTypes object from Sequelize to create our models from 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model from the Sequelize constructor
+class Post extends Model {}
+
+// initialize the model's data and configuration by passing in two objects as arguments
+Post.init(
+    // first argument: defines table columns and configuration
+    {
+        // define an id column 
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define a title column 
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define a post url column 
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        // define a column for the user that created the post through the user model
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    // configure the metadata, including the naming conventions
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
