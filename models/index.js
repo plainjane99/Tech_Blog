ad687321ce1d9a9c6b53323e33a56ccb34852745
Plain Jane <jane99hsu@gmail.com>
2020-11-07 12:59:12 -0800

    created model associations

--- models/index.js
+++ models/index.js
@@ -1,3 +1,35 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-module.exports = { User, Post };
+// a user can have many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// a post belongs to a single user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// a comment belongs to a single user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// a comment belongs to a single post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+// a user can have many comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+// a post can have many comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
