5d2e9bdbdb8faf253624e3571ad95a21a0c45cb8
Plain Jane <jane99hsu@gmail.com>
2020-11-08 14:51:41 -0800

    added add-comment functionality

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -18,20 +18,19 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     // check the session
     // ensures that only logged-in users interact with the database
-    // if (req.session) {
+    if (req.session) {
         Comment.create({
             comment_text: req.body.comment_text,
             post_id: req.body.post_id,
             // use the id from the session
-            // user_id: req.session.user_id
-            user_id: req.body.user_id
+            user_id: req.session.user_id
         })
             .then(dbCommentData => res.json(dbCommentData))
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
             });
-    // }
+    }
 });
 
 router.delete('/:id', (req, res) => {
